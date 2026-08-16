dd64fc9401e8ba1216a1b40c7f41968548979935
Rebeca Araújo <rebecaaraujo2013@gmail.com>
2026-08-15 21:08:50 -0300

    replace hardcoded profiles by map the profiles object

--- react-fundamentals/src/App.jsx
+++ react-fundamentals/src/App.jsx
@@ -3,12 +3,21 @@ import { Card } from './components/profile-card'
 import { Navbar } from './components/navbar'
 
 function App() {
+  const profiles = [
+    {name:"Mark", title:"Monkey Mesmerizer", bio:"I like to make monkeys feel home, so they can perform in a comfortable way"},
+    {name:"Claudia", title:"Snake sitter", bio:"Snakes are amazing, lovely and trustworthy animals"},
+    {name:"Junior", title:"Just a junior at the zoo", bio:"Tbh I don't know what is going on in this place"}
+  ]
   return (
-    <>
-      <Navbar/>
-      <Card name = "Carlos" title="software engineer" bio="principal software engineer at Paypal"/>
-      <Card name = "Carlos" title="software engineer" bio="principal software engineer at Paypal"/>
-    </>
+    <div className="flex-container">
+      {profiles.map( (profile) => (
+        <Card 
+          name={profile.name}
+          title={profile.title} 
+          bio={profile.bio}
+        />
+      ))}
+    </div>
   )
 }
 
